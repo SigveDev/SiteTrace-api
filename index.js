6dd76fab3b562d9e60c88df17f41ea8e1ffc03dd
2derp4you <iam2derp4you@gmail.com>
2024-08-03 23:56:50 +0200

    removed trying to return a 201 instantly when updating, and keeping it the way it was

--- index.js
+++ index.js
@@ -58,200 +58,196 @@ app.post("/analytics", async (req, res) => {
     );
 
     if (existingDocuments.total > 0) {
-      res.status(201).json({ success: true, response: "Data received" });
+      // Update the existing document
+      const documentId = existingDocuments.documents[0].$id;
+      await databases.updateDocument(
+        "669ec60f003b49ce1606",
+        "66a2d2a0003a5153d113",
+        existingDocuments.documents[0].browser.$id,
+        {
+          name: browser.name,
+          version: browser.version,
+        }
+      );
+      await databases.updateDocument(
+        "669ec60f003b49ce1606",
+        "66a2d4410005fece34aa",
+        existingDocuments.documents[0].network.$id,
+        {
+          effectiveType: network.effectiveType,
+          downlink: Number(floatToIntWithMinOne(network.downlink)),
+          rtt: network.rtt,
+        }
+      );
+      const response = await databases.updateDocument(
+        "669ec60f003b49ce1606",
+        "66a2d232000302822fc1",
+        documentId,
+        {
+          userAgent,
+          device,
+          visitDuration,
+          clicks: existingDocuments.documents[0].clicks + clicks,
+          scrollDepth: existingDocuments.documents[0].scrollDepth + scrollDepth,
+          screenResolution,
+          viewportSize,
+          loadTime,
+          focus,
+        }
+      );
 
-      (async () => {
-        // Update the existing document
-        const documentId = existingDocuments.documents[0].$id;
+      const totalAnalyticsRequest = await databases.listDocuments(
+        "66a67e300033058839e7",
+        "66abf41700087eac9f08",
+        [Query.contains("url", [url])]
+      );
+      if (totalAnalyticsRequest.total > 0) {
+        const analyticsDocumentId = totalAnalyticsRequest.documents[0].$id;
+        const totalAnalytics = totalAnalyticsRequest.documents[0];
         await databases.updateDocument(
-          "669ec60f003b49ce1606",
-          "66a2d2a0003a5153d113",
-          existingDocuments.documents[0].browser.$id,
-          {
-            name: browser.name,
-            version: browser.version,
-          }
-        );
-        await databases.updateDocument(
-          "669ec60f003b49ce1606",
-          "66a2d4410005fece34aa",
-          existingDocuments.documents[0].network.$id,
-          {
-            effectiveType: network.effectiveType,
-            downlink: Number(floatToIntWithMinOne(network.downlink)),
-            rtt: network.rtt,
-          }
-        );
-        const response = await databases.updateDocument(
-          "669ec60f003b49ce1606",
-          "66a2d232000302822fc1",
-          documentId,
-          {
-            userAgent,
-            device,
-            visitDuration,
-            clicks: existingDocuments.documents[0].clicks + clicks,
-            scrollDepth:
-              existingDocuments.documents[0].scrollDepth + scrollDepth,
-            screenResolution,
-            viewportSize,
-            loadTime,
-            focus,
-          }
-        );
-
-        const totalAnalyticsRequest = await databases.listDocuments(
           "66a67e300033058839e7",
           "66abf41700087eac9f08",
-          [Query.contains("url", [url])]
+          analyticsDocumentId,
+          {
+            interactions: totalAnalytics.interactions + clicks + scrollDepth,
+          }
         );
-        if (totalAnalyticsRequest.total > 0) {
-          const analyticsDocumentId = totalAnalyticsRequest.documents[0].$id;
-          const totalAnalytics = totalAnalyticsRequest.documents[0];
-          await databases.updateDocument(
-            "66a67e300033058839e7",
-            "66abf41700087eac9f08",
-            analyticsDocumentId,
-            {
-              interactions: totalAnalytics.interactions + clicks + scrollDepth,
-            }
+
+        const browserAnalyticsRequest =
+          totalAnalyticsRequest.documents[0].topBrowser.find(
+            (topBrowser) => topBrowser.name === browser.name
           );
 
-          const browserAnalyticsRequest =
-            totalAnalyticsRequest.documents[0].topBrowser.find(
-              (topBrowser) => topBrowser.name === browser.name
-            );
+        if (!browserAnalyticsRequest) {
+          await databases.createDocument(
+            "66a67e300033058839e7",
+            "66abf4c50019a8548c6c",
+            ID.unique(),
+            {
+              analytics: analyticsDocumentId,
+              name: browser.name,
+              amount: 1,
+            }
+          );
+        }
 
-          if (!browserAnalyticsRequest) {
-            await databases.createDocument(
-              "66a67e300033058839e7",
-              "66abf4c50019a8548c6c",
-              ID.unique(),
-              {
-                analytics: analyticsDocumentId,
-                name: browser.name,
-                amount: 1,
-              }
-            );
-          }
+        const referrerAnalyticsRequest =
+          totalAnalyticsRequest.documents[0].topReferrer.find(
+            (topReferrer) => topReferrer.name === referrer
+          );
 
-          const referrerAnalyticsRequest =
-            totalAnalyticsRequest.documents[0].topReferrer.find(
-              (topReferrer) => topReferrer.name === referrer
-            );
+        if (!referrerAnalyticsRequest) {
+          await databases.createDocument(
+            "66a67e300033058839e7",
+            "66abf51e003195178367",
+            ID.unique(),
+            {
+              analytics: analyticsDocumentId,
+              name: referrer,
+              amount: 1,
+            }
+          );
+        }
 
-          if (!referrerAnalyticsRequest) {
-            await databases.createDocument(
-              "66a67e300033058839e7",
-              "66abf51e003195178367",
-              ID.unique(),
-              {
-                analytics: analyticsDocumentId,
-                name: referrer,
-                amount: 1,
-              }
-            );
-          }
+        const deviceAnalyticsRequest =
+          totalAnalyticsRequest.documents[0].topDevice.find(
+            (topDevice) => topDevice.name === device
+          );
 
-          const deviceAnalyticsRequest =
+        if (
+          existingDocuments.documents[0].device === "Unknown" &&
+          device !== "Unknown"
+        ) {
+          const documentId = deviceAnalyticsRequest.$id;
+          await databases.updateDocument(
+            "66a67e300033058839e7",
+            "66abf5990001ff3009a9",
+            documentId,
+            {
+              amount: deviceAnalyticsRequest.amount + 1,
+            }
+          );
+        } else if (!deviceAnalyticsRequest) {
+          await databases.createDocument(
+            "66a67e300033058839e7",
+            "66abf5990001ff3009a9",
+            ID.unique(),
+            {
+              analytics: analyticsDocumentId,
+              name: device,
+              amount: 1,
+            }
+          );
+        }
+
+        if (removeOneFromUnknownDevice) {
+          const unknownDeviceAnalyticsRequest =
             totalAnalyticsRequest.documents[0].topDevice.find(
-              (topDevice) => topDevice.name === device
+              (topDevice) => topDevice.name === "Unknown"
             );
 
-          if (
-            existingDocuments.documents[0].device === "Unknown" &&
-            device !== "Unknown"
-          ) {
-            const documentId = deviceAnalyticsRequest.$id;
+          if (unknownDeviceAnalyticsRequest) {
+            const documentId = unknownDeviceAnalyticsRequest.$id;
             await databases.updateDocument(
               "66a67e300033058839e7",
               "66abf5990001ff3009a9",
               documentId,
               {
-                amount: deviceAnalyticsRequest.amount + 1,
-              }
-            );
-          } else if (!deviceAnalyticsRequest) {
-            await databases.createDocument(
-              "66a67e300033058839e7",
-              "66abf5990001ff3009a9",
-              ID.unique(),
-              {
-                analytics: analyticsDocumentId,
-                name: device,
-                amount: 1,
+                amount: unknownDeviceAnalyticsRequest.amount - 1,
               }
             );
           }
+        }
 
-          if (removeOneFromUnknownDevice) {
-            const unknownDeviceAnalyticsRequest =
-              totalAnalyticsRequest.documents[0].topDevice.find(
-                (topDevice) => topDevice.name === "Unknown"
-              );
+        const correctedTimestamp = new Date(timestamp).setHours(0, 0, 0, 0);
+        const correctedTimestampDate = new Date(correctedTimestamp);
 
-            if (unknownDeviceAnalyticsRequest) {
-              const documentId = unknownDeviceAnalyticsRequest.$id;
-              await databases.updateDocument(
-                "66a67e300033058839e7",
-                "66abf5990001ff3009a9",
-                documentId,
-                {
-                  amount: unknownDeviceAnalyticsRequest.amount - 1,
-                }
-              );
-            }
-          }
+        const analyticsOverTimeRequest = totalAnalytics.analyticsOverTime.find(
+          (analytics) =>
+            new Date(analytics.datetime).getTime() ===
+            correctedTimestampDate.getTime()
+        );
 
-          const correctedTimestamp = new Date(timestamp).setHours(0, 0, 0, 0);
-          const correctedTimestampDate = new Date(correctedTimestamp);
-
-          const analyticsOverTimeRequest =
-            totalAnalytics.analyticsOverTime.find(
-              (analytics) =>
-                new Date(analytics.datetime).getTime() ===
-                correctedTimestampDate.getTime()
-            );
-
-          if (!analyticsOverTimeRequest) {
-            await databases.createDocument(
-              "66a67e300033058839e7",
-              "66ad3d9a00305e53d6f0",
-              ID.unique(),
-              {
-                analytics: analyticsDocumentId,
-                datetime: correctedTimestampDate,
-                views: 1,
-                interactions: clicks + scrollDepth,
-              }
-            );
-          }
-        } else {
-          const correctedTimestamp = new Date(timestamp).setHours(0, 0, 0, 0);
-          const correctedTimestampDate = new Date(correctedTimestamp);
-
+        if (!analyticsOverTimeRequest) {
           await databases.createDocument(
             "66a67e300033058839e7",
-            "66abf41700087eac9f08",
+            "66ad3d9a00305e53d6f0",
             ID.unique(),
             {
-              url,
+              analytics: analyticsDocumentId,
+              datetime: correctedTimestampDate,
               views: 1,
               interactions: clicks + scrollDepth,
-              topBrowser: [{ name: browser.name, amount: 1 }],
-              topReferrer: [{ name: referrer, amount: 1 }],
-              topDevice: [{ name: device, amount: 1 }],
-              analyticsOverTime: [
-                {
-                  datetime: correctedTimestampDate,
-                  views: 1,
-                  interactions: clicks + scrollDepth,
-                },
-              ],
             }
           );
         }
-      })();
+      } else {
+        const correctedTimestamp = new Date(timestamp).setHours(0, 0, 0, 0);
+        const correctedTimestampDate = new Date(correctedTimestamp);
+
+        await databases.createDocument(
+          "66a67e300033058839e7",
+          "66abf41700087eac9f08",
+          ID.unique(),
+          {
+            url,
+            views: 1,
+            interactions: clicks + scrollDepth,
+            topBrowser: [{ name: browser.name, amount: 1 }],
+            topReferrer: [{ name: referrer, amount: 1 }],
+            topDevice: [{ name: device, amount: 1 }],
+            analyticsOverTime: [
+              {
+                datetime: correctedTimestampDate,
+                views: 1,
+                interactions: clicks + scrollDepth,
+              },
+            ],
+          }
+        );
+      }
+
+      res.status(200).json({ success: true, response });
     } else {
       // Create a new document
       const response = await databases.createDocument(
